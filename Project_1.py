344499825d86e3d729ad64c809c261c5d6c2d798
pedramsalimi <salimi.733726@gmail.com>
2018-12-21 16:05:45 +0330

    Euler Project - 1

---
+++ Project_1.py
@@ -0,0 +1,11 @@
+## TODO: If we list all the natural numbers below 10 that are multiples of
+## 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+## Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+def project_one():
+
+    result = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+    return result
+
+print(project_one())
